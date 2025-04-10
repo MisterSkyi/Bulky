e87cfb0801769a3af614128a01a1f6afe69ce379
Alex.Keegan <Alex.Keegan@T606-15>
2025-04-11 11:30:25 +1200

    Implemented: Product page   - Create   - Delete   - Edit   - Index (product home) - Added link from produts to categories

--- SD7501Bulky.DataAccess/Repository/IRepository/IProductRepository.cs
+++ SD7501Bulky.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BulkyWeb.Models;
+using SD7501Bulky.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,8 @@ using System.Threading.Tasks;
 
 namespace SD7501Bulky.DataAccess.Repository.IRepository
 {
-    public interface IUnitOfWork
+    public interface IProductRepository:IRepository<Product>
     {
-        ICategoryRepository Category { get; }
-        IProductRepository Product { get; }
-        void Save();
+        void Update(Product obj);
     }
 }
