29521962f1db084453396a326c9b4029c8a184b7
Alex.Keegan <Alex.Keegan@T606-15>
2025-04-11 09:57:44 +1200

    Implemented: Admin Area Customer Area Dropdown for content management

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -38,7 +38,7 @@ namespace BulkyWeb
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
